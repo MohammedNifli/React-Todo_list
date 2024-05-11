a5fbe2a892508e2f027278a38d4ec717cd654441
Mohammed Nifli <mohammednifliap@gmail.com>
2024-05-11 15:14:49 +0530

    completed-todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,74 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const Ff=()=>{
-    return "hello world";
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editId,setEditId]=useState(0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(editId){
+      const editTodo=todos.find((t)=>t.id===editId);
+      const updatedTodos = todos.map((t) =>
+        t.id === editTodo.id ? { ...t, todo: todo } : t
+      );
+      setTodos(updatedTodos);
+      setEditId(0);
+      setTodo("");
+      return;
+    }
+    if (todo !== "") {
+      setTodos([{ id: uuidv4(), todo }, ...todos]);
+      setTodo(""); // Clear the input field after adding a todo
+    }
+  };
+
+  const handleDelete=(id)=>{
+    const delTodo=todos.filter((to)=>to.id!==id);
+    setTodos([...delTodo]);
   }
 
+  const handleEdit=(id)=>{
+    const editTodo=todos.find((i)=>i.id===id);
+
+    setTodo(editTodo.todo);
+    setEditId(id)
+
+  }
+
+
+
   return (
-
-  <div className='App'>
-    <div className='container'>
-      <h1 id='heading'>Todo List App</h1>
-
-      
-      <form action="" className='todoForm'>
-        <input type="text" />
-        <button>Go</button>
-      </form>
-      <ul className='allTodo'>
-        
-        <li className='singleTodo'> 
-
-          <span className='todoText'>Learn React</span>
-          <button>Edit</button>
-          <button>Delete</button>
-          
-        
-        </li>
-      </ul>
-
-
+    <div className="App">
+      <div className="container">
+        <h1>Todo List App</h1>
+        <form className="todoForm" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter a todo"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button type="submit"> {editId?"Edit":"Go"}</button>
+        </form>
+        <ul className="allTodos">
+          {todos.map((t) => (
+            <li key={t.id} className="singleTodo">
+              <span className="todoText" key={t.id}>{t.todo}</span>
+              <div className="todoActions">
+                <button className="editBtn" onClick={()=>{handleEdit(t.id)}}>Edit</button>
+                <button className="deleteBtn" onClick={()=>{handleDelete(t.id)}}>Delete</button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  </div>
-    
-  )
+  );
 }
 
 export default App
